2419db17c4d3b9f4dd49ffd151b8619bc516dadc
plantstoen <plantstoen@gmail.com>
2020-11-25 00:36:42 +0900

    Add: vial-solid for SVG testing

--- src/Icon/svg/index.ts
+++ src/Icon/svg/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as community } from './community.svg';
 export { ReactComponent as trophy } from './trophy.svg';
 export { ReactComponent as gameController } from './gameController.svg';
+export { ReactComponent as vialSolid } from "./vial-solid.svg"
