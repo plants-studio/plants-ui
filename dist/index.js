517b340d7d21578decdac0a6b7e4f7de47a5f29b
plantstoen <plantstoen@gmail.com>
2020-11-25 00:41:56 +0900

    Add: semicolon in svg/index.d.ts for bundling

--- dist/index.js
+++ dist/index.js
@@ -108,8 +108,7 @@ function NavButton(_ref) {
   return jsx("div", {
     css: [areaStyle, {
       width: width
-    }],
-    onMouseO: true
+    }]
   }, jsx("div", {
     className: "item",
     css: itemStyle
@@ -178,11 +177,30 @@ function SvgGameController(props) {
   }, props), _ref$2);
 }
 
+function _extends$3() { _extends$3 = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends$3.apply(this, arguments); }
+
+var _ref$3 = /*#__PURE__*/createElement("path", {
+  fill: "currentColor",
+  d: "M477.7 186.1L309.5 18.3c-3.1-3.1-8.2-3.1-11.3 0l-34 33.9c-3.1 3.1-3.1 8.2 0 11.3l11.2 11.1L33 316.5c-38.8 38.7-45.1 102-9.4 143.5 20.6 24 49.5 36 78.4 35.9 26.4 0 52.8-10 72.9-30.1l246.3-245.7 11.2 11.1c3.1 3.1 8.2 3.1 11.3 0l34-33.9c3.1-3 3.1-8.1 0-11.2zM318 256H161l148-147.7 78.5 78.3L318 256z"
+});
+
+function SvgVialSolid(props) {
+  return /*#__PURE__*/createElement("svg", _extends$3({
+    "aria-hidden": "true",
+    "data-prefix": "fas",
+    "data-icon": "vial",
+    className: "vial-solid_svg__svg-inline--fa vial-solid_svg__fa-vial vial-solid_svg__fa-w-15",
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 480 512"
+  }, props), _ref$3);
+}
+
 var icons = /*#__PURE__*/Object.freeze({
   __proto__: null,
   community: SvgCommunity,
   trophy: SvgTrophy,
-  gameController: SvgGameController
+  gameController: SvgGameController,
+  vialSolid: SvgVialSolid
 });
 
 /** @jsx jsx */
