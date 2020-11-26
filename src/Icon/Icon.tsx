9d166244f813f65633f30f977a9c3efe035e75f2
Taeyeon <ruadl034@naver.com>
2020-11-26 19:23:25 +0900

    refactor: Icon.tsx

--- src/Icon/Icon.tsx
+++ src/Icon/Icon.tsx
@@ -1,9 +1,9 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
 import * as icons from "./svg";
+import { IconType } from "../util/Type";
 
-type IconType = keyof typeof icons;
-export const iconTypes: IconType[] = Object.keys(icons) as any[]; // 스토리에서 불러오기 위함
+// export const iconTypes: IconType[] = Object.keys(icons) as any[]; // 스토리에서 불러오기 위함
 
 export type IconProps = {
   /** 사용 할 아이콘 타입 */
