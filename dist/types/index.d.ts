2a9818799573528f82241a14efdeeb27801077fb
plantstoen <plantstoen@gmail.com>
2020-11-28 01:40:42 +0900

    Fix : add export Compoents in src/index.ts

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,3 +1,10 @@
-export { default as Button } from './button/basic-button/Button';
+export { default as Button } from "./button/basic-button/Button";
 export { default as NavButton } from "./button/nav-button/NavButton";
-export { default as Icon } from './Icon/Icon';
+export { default as IconButton } from "./button/icon-button/IconButton";
+export { default as Icon } from "./Icon/Icon";
+export { default as Card } from "./card/Card";
+export { default as Sticker } from "./sticker/Sticker";
+export { default as ContentType } from "./text/content-type/ContentType";
+export { default as Header } from "./text/header/Header";
+export { default as SubHeader } from "./text/sub-header/SubHeader";
+export { default as Progress } from "./visibility/progress/Progress.stories";
