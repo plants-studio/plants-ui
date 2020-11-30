5a0b2803e36f140894b0f8eec5d4806158207804
plantstoen <plantstoen@gmail.com>
2020-12-01 02:35:46 +0900

    Add : setOverflow, setWordKeepMode in SubHeader.tsx, set small's font-size 1rem to 0.875rem

--- src/text/sub-header/SubHeader.tsx
+++ src/text/sub-header/SubHeader.tsx
@@ -8,11 +8,33 @@ export type SubHeaderProps = {
   width?: string | number;
   /** 서브 헤더의 크기 */
   size: "small";
+  /** 오버플로우 모드 활성화 여부 */
+  isOverflowMode?: boolean;
+  /** Word-Keep 모드 활성화 여부 */
+  isWordKeepMode?: boolean;
 };
 
 /** `SubHeader` 는 보조적인 설명이 있는 헤더입니다 */
-function subHeader({ children, width, size = "small" }: SubHeaderProps) {
-  return <h3 css={[defaultStyle, sizes[size], { width }]}>{children}</h3>;
+function subHeader({
+  children,
+  width,
+  size = "small",
+  isOverflowMode = false,
+  isWordKeepMode = true,
+}: SubHeaderProps) {
+  return (
+    <h3
+      css={[
+        defaultStyle,
+        sizes[size],
+        { width },
+        setOverflow(isOverflowMode),
+        setWordKeepMode(isWordKeepMode),
+      ]}
+    >
+      {children}
+    </h3>
+  );
 }
 
 const defaultStyle = css`
@@ -25,8 +47,31 @@ const defaultStyle = css`
 
 const sizes = {
   small: css`
-    font-size: 1rem;
+    font-size: 0.875rem;
   `,
+};
+
+const setOverflow = (isOverflowMode: boolean) => {
+  if (isOverflowMode) {
+    return css`
+      display: block;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    `;
+  } else {
+    return css``;
+  }
+};
+
+const setWordKeepMode = (isWordKeepMode: boolean) => {
+  if (isWordKeepMode) {
+    return css`
+      word-break: keep-all;
+    `;
+  } else {
+    return css``;
+  }
 };
 
 export default subHeader;
